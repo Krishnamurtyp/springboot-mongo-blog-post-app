bbe62ce2b3de7cebd57b97ccdce7546d1093604c
jonasmachados <jonasmachado.ti@gmail.com>
2021-02-14 14:42:15 -0300

    Getting a post by ID

---
+++ src/main/java/com/workshopmongo.services/PostService.java
@@ -0,0 +1,26 @@
+package com.workshopmongo;
+
+import com.workshopmongo.repository.PostRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Jonas created 14/02/2021
+ */
+//Classe é im servico e pode ser injetado em outras Classes 
+@Service
+public class PostService {
+
+    //Injecao automatica do Spring
+    @Autowired
+    private PostRepository repo;
+
+    //Method that found user by ID and handling exception ID incorrect 
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
---
+++ src/main/java/com/workshopmongo/resource/PostResource.java
@@ -0,0 +1,38 @@
+package com.workshopmongo.resource;
+
+import com.workshopmongo.Post;
+import com.workshopmongo.PostService;
+import com.workshopmongo.User;
+import com.workshopmongo.UserDTO;
+import com.workshopmongo.UserService;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+/**
+ *
+ * @author Jonas created 14/02/2021
+ */
+//COntrolador Rest
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    //EndPoint that return user by your ID
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
